b7a36a18d6f0558bc75cdb5f48336b3932f7bd70
hyperrixel <50220578+hyperrixel@users.noreply.github.com>
2021-02-07 13:57:15 +0100

    Delete unit_test.py from bad location

--- unit_test.py
+++
@@ -1,8 +0,0 @@
-print('Begin unit test')
-
-if 'unit' == 'unit':
-    print('Unit test was completly successful. :)')
-else:
-    print('Unit test was a complete failure. :(')
-
-print('End of unit test')
