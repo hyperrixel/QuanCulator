ca0fd1c1ec0d8e59e08680c3ffad0e6a53bab5a9
hyperrixel <50220578+hyperrixel@users.noreply.github.com>
2021-02-07 13:54:32 +0100

    Adding example, requirements and a funny unit test

---
+++ unit_test.py
@@ -0,0 +1,8 @@
+print('Begin unit test')
+
+if 'unit' == 'unit':
+    print('Unit test was completly successful. :)')
+else:
+    print('Unit test was a complete failure. :(')
+
+print('End of unit test')
